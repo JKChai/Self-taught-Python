ce2bb0f1769a45eb591e0a455082a659b033ff99
JKChai <jk.chai@okstate.edu>
2022-02-24 00:20:32 -0600

    Update the scripts

--- mapper.py
+++ mapper.py
@@ -6,44 +6,48 @@
 # CS 5123
 # Due FEB 27
 
+## import modules
 import os
 import sys
+import re
 
-### make change to stop words
-# stop_words = "‘ourselves’, ‘hers’, ‘between’, ‘yourself’, ‘but’, ‘again’, ‘there’, " + \ 
-#             "‘about’, ‘once’, ‘during’, ‘out’, ‘very’, ‘having’, ‘with’, ‘they’, " + \
-#             "‘own’, ‘an’, ‘be’, ‘some’, ‘for’, ‘do’, ‘its’, ‘yours’, ‘such’, ‘into’, " + \
-#             "‘of’, ‘most’, ‘itself’, ‘other’, ‘off’, ‘is’, ‘s’, ‘am’, ‘or’, ‘who’, " + \
-#             "‘as’, ‘from’, ‘him’, ‘each’, ‘the’, ‘themselves’, ‘until’, ‘below’, " + \
-#             "‘are’, ‘we’, ‘these’, ‘your’, ‘his’, ‘through’, ‘don’, ‘nor’, ‘me’, " + \
-#             "‘were’, ‘her’, ‘more’, ‘himself’, ‘this’, ‘down’, ‘should’, ‘our’, " + \
-#             "‘their’, ‘while’, ‘above’, ‘both’, ‘up’, ‘to’, ‘ours’, ‘had’, ‘she’, " + \
-#             "‘all’, ‘no’, ‘when’, ‘at’, ‘any’, ‘before’, ‘them’, ‘same’, ‘and’, " + \
-#             "‘been’, ‘have’, ‘in’, ‘will’, ‘on’, ‘does’, ‘yourselves’, ‘then’, " + \
-#             "‘that’, ‘because’, ‘what’, ‘over’, ‘why’, ‘so’, ‘can’, ‘did’, ‘not’, " + \
-#             "‘now’, ‘under’, ‘he’, ‘you’, ‘herself’, ‘has’, ‘just’, ‘where’, " + \
-#             "‘too’, ‘only’, ‘myself’, ‘which’, ‘those’, ‘i’, ‘after’, ‘few’, " + \
-#             "‘whom’, ‘t’, ‘being’, ‘if’, ‘theirs’, ‘my’, ‘against’, ‘a’, ‘by’, " + \
-#             "‘doing’, ‘it’, ‘how’, ‘further’, ‘was’, ‘here’, ‘than’"
-### 
-# stop_words = stop_words.replace(u"\u2018", "").replace(u"\u2019", "")
-# stop_words = set(stop_words.split(', '))
-
+## hard code stop-words
 stop_words = {'just', 'while', 'me', 'further', 'no', 'any', 'up', 
             'whom', 'above', 'a', 'are', 'the', 'where', 'him', 'our', 
             'off', 'themselves', 'their', 'once', 'don', 'on', 'against', 
             'them', 'hers', 'after', 'will', 'himself', 'who', 'there', 
             'be', 'or', 'an', 'few', 'in', 'until', 'same', 'my', 'then', 
-            'some', 'because', 'having', 'you', 'own', 'here', 'ours', 'does', 
-            'he', 'below', 'she', 'only', 'out', 'again', 'most', 'its', 'his', 
-            'being', 'theirs', 'been', 'is', 'ourselves', 'during', 'has', 'for', 
-            'each', 'now', 'herself', 'very', 'when', 'that', 'did', 'am', 'yours', 
-            'this', 'too', 'about', 'by', 'can', 'nor', 'yourselves', 'how', 'between', 
-            'both', 'i', 'at', 'so', 'into', 'as', 'those', 'myself', 'but', 'should', 
-            't', 'other', 'under', 'have', 'through', 'your', 'not', 'down', 'we', 
-            'which', 'of', 'had', 'itself', 'yourself', 's', 'doing', 'before', 
-            'what', 'do', 'and', 'her', 'to', 'all', 'from', 'than', 'these', 'more', 
+            'some', 'because', 'having', 'you', 'own', 'here', 'ours', 
+            'does', 'he', 'below', 'she', 'only', 'out', 'again', 'most', 
+            'its', 'his', 'being', 'theirs', 'been', 'is', 'ourselves', 
+            'during', 'has', 'for', 'each', 'now', 'herself', 'very', 'when', 
+            'that', 'did', 'am', 'yours', 'this', 'too', 'about', 'by', 'can', 
+            'nor', 'yourselves', 'how', 'between', 'both', 'i', 'at', 'so', 
+            'into', 'as', 'those', 'myself', 'but', 'should', 't', 'other', 
+            'under', 'have', 'through', 'your', 'not', 'down', 'we', 'which', 
+            'of', 'had', 'itself', 'yourself', 's', 'doing', 'before', 'what', 
+            'do', 'and', 'her', 'to', 'all', 'from', 'than', 'these', 'more', 
             'they', 'were', 'why', 'if', 'such', 'it', 'over', 'with', 'was'}
 
+## collect each line from the given file
+for line in sys.stdin:
 
+    ## remove all whitespace 
+    line = line.strip()
+    ## split line into list based on whitespace
+    words = line.split()
 
+    ## iterate each word from list of words
+    ## this loop does 3 things
+    ## 1. Convert uppercase to lowercase
+    ## 2. Remove non-alphabetical characters
+    ## 3. Prevent stopwords to output
+    for word in words:
+        ## convert upper case to lower case
+        word = word.lower()
+        ## only keep alphabetical character
+        word = re.sub("[^a-zA-Z]+", '', word)
+        ## prevent stop words from output
+        if word not in stop_words:
+            ## output key-value as (word, 1) 
+            print('%s\t%s' % (word, 1))
